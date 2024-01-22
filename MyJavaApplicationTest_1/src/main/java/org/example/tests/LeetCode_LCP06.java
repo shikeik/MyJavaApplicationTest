19cf730aaf56d90bd7bb17e21d06a770239c5136
shikeik <30600841+shikeik@users.noreply.github.com>
2024-01-22 23:23:47 +0800

    这个项目变成测试代码库了.

--- MyJavaApplicationTest_1/src/main/java/org/example/tests/LeetCode_LCP06.java
+++ MyJavaApplicationTest_1/src/main/java/org/example/tests/LeetCode_LCP06.java
@@ -2,6 +2,13 @@ package org.example.tests;
 
 import com.goldsprite.myDebugger.DM;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * leetcode解题<br>
  * <a href="https://leetcode.cn/problems/na-ying-bi/">原文链接</a>
@@ -12,8 +19,56 @@ public class LeetCode_LCP06 {
 
     public LeetCode_LCP06(){
 
-        int maxtick = new Solution().minCount(coins);
-        DM.WL("maxtick: "+maxtick);
+//        base64();
+//        m1();
+    }
+
+
+    /**
+     * base64互转
+     */
+    private void base64() {
+        //base64编码
+        String originalString = "Hello, Base64!";
+        String encodedString = Base64.getEncoder().encodeToString(originalString.getBytes());
+        System.out.println("Original String: " + originalString);
+        System.out.println("Encoded String: " + encodedString);
+
+        String encodedString2 = "SGVsbG8sIEJhc2U2NCE=";
+        // Base64解码
+        byte[] decodedBytes = Base64.getDecoder().decode(encodedString2);
+        String decodedString = new String(decodedBytes);
+        System.out.println("Encoded String: " + encodedString2);
+        System.out.println("Decoded String: " + decodedString);
+    }
+
+    /**
+     * 遍历中删除map元素
+     */
+    private void m1() {
+        Map<String, Integer> k = new HashMap<>();
+        k.put("AA", 1);
+        k.put("BB", 3);
+        k.put("CC", 2);
+
+        Iterator<Map.Entry<String, Integer>> iterator = k.entrySet().iterator();
+        while(iterator.hasNext()){
+            Map.Entry<String, Integer> entry = iterator.next();
+            System.out.println(entry.getKey()+", "+entry.getValue());
+            iterator.remove();
+        }
+        System.out.println(k.size());
+    }
+
+
+    public static String getFileNameNoEx(String filename) {
+        if ((filename != null) && (!filename.isEmpty())) {
+            int dot = filename.lastIndexOf('.');
+            if (dot >-1) {
+                return filename.substring(0, dot);
+            }
+        }
+        return filename;
     }
 
 
